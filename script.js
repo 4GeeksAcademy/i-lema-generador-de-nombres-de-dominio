93d9ecbbb79e0805d50ed2626c3da7b10337657d
i-lema <161314957+i-lema@users.noreply.github.com>
2024-04-26 19:31:34 +0000

    añadir script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+// console.log("Cualquier string");
+
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+let dominio = [];
+
+// for (let i = 0; i < pronoun.length; i++) {
+//   for (let x = 0; x < adj.length; x++) {
+//     for (let y = 0; y < noun.length; y++) {
+//       dominio.push(pronoun[i] + adj[x] + noun[y] + '.com');
+//     }
+//   }
+// }
+
+// console.log(dominio);
+
+
+// pronoun.forEach((elem)=>
+//     adj.forEach((elem1)=>
+//         noun.forEach((elem2)=>
+//             dominio.push(elem+elem1+elem2+".com")
+//         )
+//     )
+// );
+// console.log(dominio);
+
+
+let domains = pronoun.map(elem =>
+    adj.map(elem1 =>
+      noun.map(elem2 =>
+        elem+elem1+elem2+"com")
+    )
+  ).flat(3);
+  console.log(domains);
--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// console.log("Cualquier string");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let domains = [];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      domains.push(pronoun[i] + adj[j] + noun[k] + '.com');
+    }
+  }
+}
+
+console.log(domains);
